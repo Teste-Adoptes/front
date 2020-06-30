2ae49bde61e7e7b809c85200136245933e107704
Bruno Sales <engbrunosales@gmail.com>
2020-06-29 21:01:53 -0300

    Adapter api.js for auth script

--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,18 @@
 import axios from 'axios';
-const Api = axios.create({baseURL: process.env.API_URL});
-export default Api;
+import { getToken } from "./auth";
+//require('dotenv').config()
+
+
+const api = axios.create({baseURL: process.env.REACT_APP_API_URL,
+                          headers: {"Access-Control-Allow-Origin": process.env.REACT_APP_URL,
+                                    "Access-Control-Allow-Credentials": 'true'}});
+
+api.interceptors.request.use(async config => {
+  const token = getToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
