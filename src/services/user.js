2ff9142e4007ab49548e18f0fecfc3201235cc59
Bruno Sales <engbrunosales@gmail.com>
2020-06-29 21:02:24 -0300

    Refactory service for user

--- src/services/user.js
+++ src/services/user.js
@@ -1,12 +1,12 @@
 import Api from './api';
 
-const url_base = `${process.env.API_URL}/cadastro`;
+const url = 'cadastro';
 
 const UserService = {
-  get: (id) => Api.get(`${url_base}/${id}`),
-  post: (data) => Api.post(`${url_base}`, data),
-  put: (id, data) => Api.put(`${url_base}/${id}`, data),
-  delete: (id) => Api.delete(`${url_base}/${id}`)                       
+  get: (id) => Api.get(`${url}/${id}`),
+  post: (data) => Api.post(`${url}`, data),
+  put: (id, data) => Api.put(`${url}/${id}`, data),
+  delete: (id) => Api.delete(`${url}/${id}`)                       
 }
  
 export default UserService;
